205360d27dde9423542a1a9772a0d876af946926
Uwe Geercken <uwe.geercken@web.de>
2024-01-02 00:00:11 +0100

    added junit tests

--- src/main/java/com/datamelt/domain/PersonService.java
+++ src/main/java/com/datamelt/domain/PersonService.java
@@ -4,8 +4,9 @@ import com.datamelt.domain.model.Person;
 import com.datamelt.port.in.person.PersonUseCase;
 import com.datamelt.port.out.PersonRepository;
 
+import javax.inject.Singleton;
 import java.util.Optional;
-
+@Singleton
 public class PersonService implements PersonUseCase
 {
     private final PersonRepository personRepository;
